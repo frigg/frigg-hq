b143435f89dca94d1992a31fffc1d7b31392c492
Fredrik Nygård Carlsen <me@frecar.no>
2014-04-13 16:09:26 -0400

    create Project class skeleton

--- frigg/project/models.py
+++ frigg/project/models.py
@@ -1,6 +1,34 @@
 from django.db import models
+from fabric.operations import run
+from fabric.context_managers import cd
+from frigg.settings import PROJECT_TMP_DIRECTORY
 
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
     repo_git = models.CharField(max_length=150)
+    branch = models.CharField(max_length=100, default="master")
+
+    def run_tests(self):
+
+        self._create_tmp_folder()
+        self._clone_repo()
+        self._run_tox()
+        self._run_flake8()
+        self._set_commit_status()
+        self._delete_tmp_folder()
+
+    def _clone_repo(self):
+        self._run("git clone %s" % self.repo_git)
+        self._run("git checkout %s" % self.branch)
+
+    def _run_tox(self):
+        self._run("tox")
+
+    def _run_flake8(self):
+        self._run("flake8")
+
+    def _run(self, command):
+        with cd(PROJECT_TMP_DIRECTORY + self.id):
+            run(command)
+
--- frigg/settings.py
+++ frigg/settings.py
@@ -85,3 +85,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+PROJECT_TMP_DIRECTORY = "/tmp/"
