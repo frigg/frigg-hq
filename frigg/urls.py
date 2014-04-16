8e496a5b13a1e802120895c0bb3b11f642f192e7
Fredrik Nygård Carlsen <me@frecar.no>
2014-04-16 17:59:15 -0400

    add github webhook url

--- frigg/urls.py
+++ frigg/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'frigg.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^github-webhook/', 'frigg.builds.views.github_webhook'),
     url(r'^admin/', include(admin.site.urls)),
 )
