2026bcaf5703e8698da113cb9964ff9aa8163bfa
Fredrik Nygård Carlsen <me@frecar.no>
2014-05-31 17:54:55 +0200

    Adds login template

--- frigg/urls.py
+++ frigg/urls.py
@@ -15,4 +15,8 @@ urlpatterns = patterns('',
 
     url(r'^github-webhook/', 'frigg.builds.views.github_webhook'),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
+
 )
