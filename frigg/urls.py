0534d78d183da9064c3473f57910817d37998dc4
Rolf Erik Lekang <me@rolflekang.com>
2014-09-21 18:08:53 +0200

    make the code base pep8 compliant

--- frigg/urls.py
+++ frigg/urls.py
@@ -3,20 +3,20 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'frigg.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
 
     url(r'^$', 'frigg.builds.views.overview'),
 
     url(r'^build/(?P<build_id>\d+)/$', 'frigg.builds.views.build', name="view_build"),
-    url(r'^deploy/(?P<build_id>\d+)/$', 'frigg.builds.views.deploy_master_branch', name="deploy_master_branch"),
+    url(r'^deploy/(?P<build_id>\d+)/$', 'frigg.builds.views.deploy_master_branch',
+        name="deploy_master_branch"),
 
     url(r'^github-webhook/', 'frigg.builds.views.github_webhook'),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'},
+        name='login'),
 
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
 )
