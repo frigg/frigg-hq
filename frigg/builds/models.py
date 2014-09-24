0805c629c2dc2dd6b7fa90075df29fbad2d15032
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-09-24 20:25:00 +0200

    Read comment setting from frig.yml

--- frigg/builds/models.py
+++ frigg/builds/models.py
@@ -142,7 +142,7 @@ class Build(models.Model):
                 self.result.save()
 
     def add_comment(self, message):
-        if getattr(settings, 'GITHUB_COMMENT_MESSAGE', True):
+        if bool(self.load_settings().get('comment', True)):
             owner, repo = self.get_git_repo_owner_and_name()
             url = "%s/%s/commits/%s/comments" % (owner, repo, self.sha)
             github_api_request(url, {'body': message, 'sha': self.sha})
