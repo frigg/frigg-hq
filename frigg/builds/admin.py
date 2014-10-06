583ea81b413e68d9dcaa7f7564898263cdd182da
Rolf Erik Lekang <me@rolflekang.com>
2014-10-06 20:59:08 +0200

    add Project in admin

--- frigg/builds/admin.py
+++ frigg/builds/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from frigg.builds.models import Build, BuildResult
+from frigg.builds.models import Build, BuildResult, Project
 
+admin.site.register(Project)
 admin.site.register(Build)
 admin.site.register(BuildResult)
