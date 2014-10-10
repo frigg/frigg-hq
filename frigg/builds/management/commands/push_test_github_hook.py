8700b916652e938ffc1ae1c8310dd0f0dff1a589
Rolf Erik Lekang <me@rolflekang.com>
2014-10-10 14:34:08 +0200

    fix path to fixture in mangement command #45

--- frigg/builds/management/commands/push_test_github_hook.py
+++ frigg/builds/management/commands/push_test_github_hook.py
@@ -10,7 +10,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     def handle(self, *args, **options):
         fixtures_path = os.path.join(settings.BASE_DIR, 'helpers/fixtures/github')
-        data = json.load(open(os.path.join(fixtures_path, 'push.json')))
+        data = json.load(open(os.path.join(fixtures_path, 'push_master.json')))
 
         r = requests.post("http://localhost:8000/github-webhook/",
                           data=json.dumps(data),
