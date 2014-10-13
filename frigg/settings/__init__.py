6ac8d67b9a85fd121c06470be5fe53bca92aae0b
Rolf Erik Lekang <me@rolflekang.com>
2014-10-13 09:58:32 +0200

    Change to local imports in settings.__init__

--- frigg/settings/__init__.py
+++ frigg/settings/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf8 -*-
 
 # django base settings
-from frigg.settings.base import *
+from .base import *
 
 # settings variables introduced in frigg
-from frigg.settings.frigg import *
+from .frigg import *
 
 try:
-    from frigg.settings.local import *
+    from .local import *
 except ImportError, e:
     raise ImportError("Couldn't load local settings frigg.settings.local")
