4bd0e57930959809e484573f34bf2c3797dee6b9
Rolf Erik Lekang <me@rolflekang.com>
2014-10-17 11:38:23 +0200

    Fix pep8

--- frigg/builds/views.py
+++ frigg/builds/views.py
@@ -9,13 +9,15 @@ from .models import Build, Project
 @login_required
 def overview(request):
     return render(request, "builds/overview.html", {
-        'builds': Build.objects.permitted(request.user).order_by('-id').select_related('project', 'result')[:100]
+        'builds': Build.objects.permitted(request.user).order_by('-id')
+                                                       .select_related('project', 'result')[:100]
     })
 
 
 @login_required
 def view_organization(request, owner):
-    builds = Build.objects.permitted(request.user).filter(project__owner=owner).select_related('project', 'result')
+    builds = Build.objects.permitted(request.user).filter(project__owner=owner)\
+                                                  .select_related('project', 'result')
     if len(builds) == 0:
         raise Http404
 
@@ -28,17 +30,23 @@ def view_organization(request, owner):
 @login_required
 def view_project(request, owner, name):
     return render(request, "builds/project.html", {
-        'project': get_object_or_404(Project.objects.permitted(request.user).prefetch_related('builds'),
-                                     owner=owner, name=name)
+        'project': get_object_or_404(
+            Project.objects.permitted(request.user).prefetch_related('builds'),
+            owner=owner,
+            name=name
+        )
     })
 
 
 @login_required
 def view_build(request, owner, name, build_number):
     return render(request, "builds/build.html", {
-        'build': get_object_or_404(Build.objects.permitted(request.user).select_related('project'),
-                                   project__owner=owner, project__name=name,
-                                   build_number=build_number)
+        'build': get_object_or_404(
+            Build.objects.permitted(request.user).select_related('project'),
+            project__owner=owner,
+            project__name=name,
+            build_number=build_number
+        )
     })
 
 
