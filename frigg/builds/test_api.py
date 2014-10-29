b24008051cbe5b6bb55dacc2994bd5503fcd6a01
Rolf Erik Lekang <me@rolflekang.com>
2014-10-29 10:24:27 +0100

    fix #52, Add build badges

---
+++ frigg/builds/test_api.py
@@ -0,0 +1,42 @@
+# -*- coding: utf8 -*-
+from django.contrib.staticfiles import finders
+from django.core.urlresolvers import reverse
+from django.test import TestCase, Client
+from frigg.builds.models import BuildResult
+
+
+class BadgeTestCase(TestCase):
+    fixtures = ['frigg/builds/fixtures/users.yaml', 'frigg/builds/fixtures/test_views.yaml']
+
+    def assertStatusCode(self, response, code=200):
+        self.assertEqual(response.status_code, code)
+
+    def setUp(self):
+        self.client = Client()
+        with open(finders.find('badges/build-success.svg')) as f:
+            self.success = f.read()
+        with open(finders.find('badges/build-failure.svg')) as f:
+            self.failure = f.read()
+
+    def test_success(self):
+        response = self.client.get(reverse('build_badge', args=['frigg', 'frigg']))
+        self.assertStatusCode(response)
+        self.assertEquals(response.content, self.success)
+
+        response = self.client.get(reverse('build_badge', args=['frigg', 'frigg', 'another-branch']))
+        self.assertStatusCode(response)
+        self.assertEquals(response.content, self.success)
+
+    def test_failure(self):
+        BuildResult.objects.all().update(succeeded=False)
+        response = self.client.get(reverse('build_badge', args=['frigg', 'frigg']))
+        self.assertStatusCode(response)
+        self.assertEquals(response.content, self.failure)
+
+        response = self.client.get(reverse('build_badge', args=['frigg', 'frigg', 'another-branch']))
+        self.assertStatusCode(response)
+        self.assertEquals(response.content, self.failure)
+
+    def test_404(self):
+        response = self.client.get(reverse('build_badge', args=['frigg', 'nothing']))
+        self.assertStatusCode(response, code=404)
--- frigg/urls.py
+++ frigg/urls.py
@@ -2,12 +2,20 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns(
+    'frigg.builds.api',
+    url(r'^webhooks/github/$', 'github_webhook'),
+    url(r'^api/workers/report/$', 'report_build', name='worker_api_report_build'),
+    url(r'^badges/(?P<owner>[^/]+)/(?P<project>[^/]+)/$', 'build_badge', name='build_badge'),
+    url(r'^badges/(?P<owner>[^/]+)/(?P<project>[^/]+)/(?P<branch>[^/]+)/$', 'build_badge',
+        name='build_badge'),
+)
+
+urlpatterns += patterns(
     '',
-    url(r'^webhooks/github/$', 'frigg.builds.api.github_webhook'),
-    url(r'^api/workers/report/$', 'frigg.builds.api.report_build', name='worker_api_report_build'),
     url(r'^admin/', include(admin.site.urls)),
 
     url(
