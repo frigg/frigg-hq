b301494a8eb49d64c582739f0d0dc0058b136bed
Rolf Erik Lekang <me@rolflekang.com>
2014-11-07 21:35:20 +0100

    Add webhook handling of member action

--- frigg/webhooks/views.py
+++ frigg/webhooks/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf8 -*-
 import json
+from django.contrib.auth import get_user_model
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -13,27 +14,42 @@ def github_webhook(request):
     try:
         event = request.META['HTTP_X_GITHUB_EVENT']
     except KeyError:
-        return HttpResponse("Missing HTTP_X_GITHUB_EVENT")
+        return HttpResponse('Missing HTTP_X_GITHUB_EVENT')
 
     data = json.loads(request.body)
-    if event == "issue_comment":
+    if event == 'issue_comment':
         data = github.parse_comment_payload(data)
 
         # if this comment is on a pull request, build it
-        if data and data['pull_request_url'] != "":
+        if data and data['pull_request_url'] != '':
             data = github.parse_pull_request_payload(github.api_request(
                 data['pull_request_url'],
                 Project.token_for_url(data['repo_url'])
             ))
 
-    elif event == "pull_request":
+    elif event == 'pull_request':
         data = github.parse_pull_request_payload(data)
 
-    elif event == "push":
+    elif event == 'push':
         data = github.parse_push_payload(data)
 
+    elif event == 'member':
+        if data['action'] != 'added':
+            # no other action is supported so we don't know how the payload will be
+            return HttpResponse('Unknown action')
+
+        username = data['member']['login']
+        if get_user_model().objects.filter(username=username).exists():
+            project = Project.objects.get(
+                name=data['repository']['name'],
+                owner=data['repository']['owner']['login']
+            )
+            project.members.add(get_user_model().objects.get(username=username))
+            return HttpResponse('Added %s to the project' % data['member']['login'])
+        return HttpResponse('Unknown user %s' % username)
+
     else:
-        return HttpResponse("Unknown event: %s" % event)
+        return HttpResponse('Unknown event: %s' % event)
 
     if data:
         project = Project.objects.get_or_create_from_url(data['repo_url'])
