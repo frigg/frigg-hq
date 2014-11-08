68070b066576ba183bf447db7903407b36084128
Rolf Erik Lekang <me@rolflekang.com>
2014-11-08 16:27:04 +0100

    Fix test fixture, make it use custom user

--- frigg/builds/test_views.py
+++ frigg/builds/test_views.py
@@ -7,7 +7,7 @@ from django.test import TestCase, RequestFactory
 from django.test.utils import override_settings
 
 from .api import report_build
-from frigg.builds.models import Build
+from .models import Build
 from .views import overview, view_build, view_organization, view_project
 
 
