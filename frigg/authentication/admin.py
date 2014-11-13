28ca228a7b3cffc12ea7d376d7a3551053e1bf72
Rolf Erik Lekang <me@rolflekang.com>
2014-11-13 20:38:46 +0100

    Add UserAdmin

--- frigg/authentication/admin.py
+++ frigg/authentication/admin.py
@@ -1,1 +1,10 @@
 # -*- coding: utf8 -*-
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    pass
