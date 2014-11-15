0b3074455ca5b52f46d0bab68cca01aaee518ccb
Rolf Erik Lekang <me@rolflekang.com>
2014-11-15 19:35:02 +0100

    Add auto-approval of projects based on owners

--- frigg/builds/test_models.py
+++ frigg/builds/test_models.py
@@ -15,7 +15,14 @@ class ProjectTestCase(TestCase):
         self.assertEqual(project.name, 'balder')
         self.assertEqual(project.git_repository, url)
         self.assertIsNone(project.average_time)
+        self.assertFalse(project.approved)
         self.assertEqual(Project.objects.all().count(), 1)
+
+    def test_auto_approval(self):
+        url = 'git@github.com:frigg/frigg.git'
+        project = Project.objects.get_or_create_from_url(url)
+        self.assertEqual(project.git_repository, url)
+        self.assertTrue(project.approved)
 
     def test_get_project(self):
         url = 'git@github.com:tind/balder.git'
