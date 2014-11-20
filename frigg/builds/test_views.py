e5c9cce1259c34319ff2d4dd3c39f9edcadfd6c3
Rolf Erik Lekang <me@rolflekang.com>
2014-11-21 00:14:40 +0100

    Add missing tests for builds views

--- frigg/builds/test_views.py
+++ frigg/builds/test_views.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import AnonymousUser
 from django.contrib.messages.storage.fallback import FallbackStorage
 from django.core.urlresolvers import reverse
 from django.http import Http404
-from django.test import TestCase, RequestFactory
+from django.test import TestCase, RequestFactory, Client
+from frigg.builds.views import approve_projects
 
+from .models import Build
 from .views import overview, view_build, view_organization, view_project, last_build
 
 
@@ -38,11 +40,19 @@ class SmokeTestCase(TestCase):
         response = overview(request)
         self.assertStatusCode(response)
 
+        self.add_request_fields(request, anonymous=True)
+        response = overview(request)
+        self.assertStatusCode(response)
+
     def test_organization_view(self):
-        request = self.factory.get(reverse('view_organization', args=['tind']))
+        request = self.factory.get(reverse('view_organization', args=['frigg']))
         self.add_request_fields(request)
         response = view_organization(request, 'frigg')
         self.assertStatusCode(response)
+
+        request = self.factory.get(reverse('view_organization', args=['dumbledore']))
+        self.add_request_fields(request)
+        self.assertRaises(Http404, view_organization, request, 'dumbledore')
 
     def test_project_view(self):
         request = self.factory.get(reverse('view_project', args=['frigg', 'frigg']))
@@ -66,3 +76,16 @@ class SmokeTestCase(TestCase):
         request = self.factory.get(reverse('last_build', args=['frigg', 'chewie']))
         self.add_request_fields(request, anonymous=True)
         self.assertRaises(Http404, last_build, request, 'frigg', 'chewie')
+
+    def test_approve_project_404(self):
+        response = self.client.get(reverse('approve_projects'))
+        self.assertStatusCode(response, 404)
+
+        response = self.client.get(reverse('approve_project', args=[1]))
+        self.assertStatusCode(response, 404)
+
+    def test_approve_projects_view(self):
+        request = self.factory.get(reverse('approve_projects'))
+        self.add_request_fields(request)
+        response = approve_projects(request)
+        self.assertStatusCode(response, 200)
