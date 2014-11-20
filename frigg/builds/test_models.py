46fe71f0d9b63922a69e1ef9148b5b32748b63a5
Rolf Erik Lekang <me@rolflekang.com>
2014-11-20 15:15:43 +0100

    Add test for Build.has_timed_out()

--- frigg/builds/test_models.py
+++ frigg/builds/test_models.py
@@ -1,7 +1,9 @@
 # -*- coding: utf8 -*-
+from datetime import timedelta
 from django.test import TestCase
+from django.utils.timezone import now
 
-from .models import Project, BuildResult
+from .models import Project, BuildResult, Build
 
 
 class ProjectTestCase(TestCase):
@@ -30,6 +32,26 @@ class ProjectTestCase(TestCase):
         self.assertEqual(Project.objects.all().count(), 1)
         Project.objects.get_or_create_from_url(url)
         self.assertEqual(Project.objects.all().count(), 1)
+
+
+class BuildTestCase(TestCase):
+    def tearDown(self):
+        Project.objects.all().delete()
+
+    def test_has_timed_out(self):
+        url = 'git@github.com:frigg/frigg.git'
+        project = Project.objects.get_or_create_from_url(url)
+        build = Build.objects.create(project=project, build_number=1,
+                                     start_time=now() - timedelta(minutes=15))
+        self.assertTrue(build.has_timed_out())
+        build.start_time = now()
+        self.assertFalse(build.has_timed_out())
+        project.average_time = 120
+        self.assertFalse(build.has_timed_out())
+        build.start_time = now() - timedelta(seconds=60)
+        self.assertFalse(build.has_timed_out())
+        build.start_time = now() - timedelta(seconds=260)
+        self.assertTrue(build.has_timed_out())
 
 
 class BuildResultTestCase(TestCase):
