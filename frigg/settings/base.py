79cd2513f42d5597f638c5c44103bf3e95bfb6b0
Rolf Erik Lekang <me@rolflekang.com>
2015-01-06 11:08:05 +0100

    Set postgresql as default db

--- frigg/settings/base.py
+++ frigg/settings/base.py
@@ -67,8 +67,9 @@ AUTHENTICATION_BACKENDS = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'frigg',
+        'HOST': '127.0.0.1'
     }
 }
 
