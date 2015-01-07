cb46c4789b2b6064e8077bf047d09b13e6f62141
Rolf Erik Lekang <me@rolflekang.com>
2015-01-07 21:32:21 +0100

    Run tests with sqlite

--- frigg/stats/test_views.py
+++ frigg/stats/test_views.py
@@ -1,4 +1,6 @@
 # -*- coding: utf8 -*-
+from unittest import skipIf
+from django.conf import settings
 from django.core.urlresolvers import reverse
 
 from frigg.utils.tests import ViewTestCase
@@ -19,6 +21,7 @@ class StatsSmokeViewTestCase(ViewTestCase):
         response = overview(request)
         self.assertStatusCode(response, 302)
 
+    @skipIf('sqlite' in settings.DATABASES['default']['ENGINE'], 'No date_trunc() in sqlite')
     def test_overview_staff_user(self):
         request = self.factory.get(reverse('overview'))
         self.add_request_fields(request, staff=True)
