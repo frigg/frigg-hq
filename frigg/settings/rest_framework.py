ab098aacabe941d19bd0ee25d6e436fc1d52d6e9
Rolf Erik Lekang <me@rolflekang.com>
2015-02-08 08:58:29 +0100

    Add django-rest-framework settings

---
+++ frigg/settings/rest_framework.py
@@ -0,0 +1,8 @@
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '50/day',
+    }
+}
