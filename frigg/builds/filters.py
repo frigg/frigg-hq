c7a67d4a69e1fe2ecb7f6c1a56202c6153e9766c
Rolf Erik Lekang <me@rolflekang.com>
2015-02-15 20:02:20 +0100

    Fix multiple instance bug in api

--- frigg/builds/filters.py
+++ frigg/builds/filters.py
@@ -5,9 +5,9 @@ from frigg.builds.models import Build, Project
 
 class ProjectPermissionFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        return queryset.filter(Project.objects.permitted_query(request.user))
+        return queryset.filter(Project.objects.permitted_query(request.user)).distinct()
 
 
 class BuildPermissionFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        return queryset.filter(Build.objects.permitted_query(request.user))
+        return queryset.filter(Build.objects.permitted_query(request.user)).distinct()
