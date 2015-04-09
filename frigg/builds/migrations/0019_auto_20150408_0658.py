90175fa52f084695039bfaf20ba2013ed79c045a
Rolf Erik Lekang <me@rolflekang.com>
2015-04-09 11:09:24 +0200

    Fix pep8 and isort warnings in migrations

--- frigg/builds/migrations/0019_auto_20150408_0658.py
+++ frigg/builds/migrations/0019_auto_20150408_0658.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
 from django.conf import settings
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -15,6 +15,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='project',
             name='members',
-            field=models.ManyToManyField(blank=True, to=settings.AUTH_USER_MODEL, related_name='projects'),
+            field=models.ManyToManyField(blank=True, to=settings.AUTH_USER_MODEL,
+                                         related_name='projects'),
         ),
     ]
