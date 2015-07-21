b9f40dd83a603f7457588190bd7d968e30fa74a7
Fredrik Carlsen <fredrik@carlsen.io>
2015-07-21 12:07:46 +0200

    Set drf to onyl render json response on all requests

--- frigg/settings/rest_framework.py
+++ frigg/settings/rest_framework.py
@@ -1,4 +1,7 @@
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.AnonRateThrottle',
     ),
