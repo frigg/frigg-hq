cd892467d5f0391c464aec2096442271f526db81
Rolf Erik Lekang <me@rolflekang.com>
2015-12-06 16:51:17 +0100

    Set default db name to frigg

--- frigg/settings/base.py
+++ frigg/settings/base.py
@@ -88,7 +88,7 @@ AUTHENTICATION_BACKENDS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'frigg-hq',
+        'NAME': 'frigg',
         'HOST': '127.0.0.1'
     }
 }
