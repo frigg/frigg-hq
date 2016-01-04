33bc37746177678bd803df0e104e32874987ca86
Rolf Erik Lekang <me@rolflekang.com>
2016-01-04 22:00:54 +0100

    feat: Add model for environment variables in projects

--- frigg/projects/models.py
+++ frigg/projects/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+
 from frigg.builds.models import Project  # noqa
+
+
+class EnvironmentVariable(models.Model):
+    project = models.ForeignKey(Project, related_name='environment_variables')
+    key = models.CharField(max_length=200)
+    value = models.TextField()
+    is_secret = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '{project} - {key}'.format(project=self.project, key=self.key)
