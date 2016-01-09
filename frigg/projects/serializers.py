9aa17b90b8f3413f0621cc25a686774dd809dc84
Rolf Erik Lekang <me@rolflekang.com>
2016-01-09 11:32:12 +0100

    feat: Add drf serializer for environment variables

--- frigg/projects/serializers.py
+++ frigg/projects/serializers.py
@@ -5,6 +5,24 @@ from frigg.builds.serializers import BuildInlineSerializer
 from .models import Project
 
 
+class EnvironmentVariableSerializer(serializers.ModelSerializer):
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if instance.is_secret:
+            representation.value = '[secret]'
+        return representation
+
+    class Meta:
+        model = Project
+        fields = (
+            'id',
+            'key',
+            'is_secret',
+            'value',
+        )
+
+
 class ProjectSerializer(serializers.ModelSerializer):
     builds = BuildInlineSerializer(read_only=True, many=True)
 
